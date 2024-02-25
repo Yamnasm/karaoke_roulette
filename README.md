9205a1f639c1034ffce116108ccc68facfd4c3a2
unknown <jrts220@gmail.com>
2024-02-25 22:39:45 +0000

    Formalising ideas

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 Python implmentation of a previously made VB.NET project.
 Selects random player(s) and song from a file and displays results.
+
+TODO:
+"Audience" view using PIL / Tkinter (or variant)
+ - Wallpaper
+ - coverartarchive.org API request for album cover (for selections)
+ - Configurable font / size / colour options from Host view
+ 
+"Host" view with Tkinter (or variant) options. 
--- karaoke_selector.py
+++ karaoke_selector.py
@@ -2,8 +2,8 @@ import random
 
 class Roulette:
     def __init__(self):
-        self.singers = get_singers()
-        self.songs = get_songs()
+        self.singers = self.get_singers()
+        self.songs = self.get_songs()
     
     def spin(self):
         out_dict = {
@@ -12,28 +12,28 @@ class Roulette:
         }
         return out_dict
 
-def get_singers():
-    singers_list = []
-    with open("Singers - Karaoke Roulette.txt", "r") as f:
-        for line in f.readlines():
-            singers_list.append(line.rstrip("\n"))
-    return singers_list
+    def get_singers(self):
+        singers_list = []
+        with open("Singers - Karaoke Roulette.txt", "r") as f:
+            for line in f.readlines():
+                singers_list.append(line.rstrip("\n"))
+        return singers_list
 
-def get_songs():
-    raw_song_list = []
-    with open("Song List - Karaoke Roulette.txt", "r") as f:
-        for line in f.readlines():
-            raw_song_list.append(line.rstrip("\n"))
+    def get_songs(self):
+        raw_song_list = []
+        with open("Song List - Karaoke Roulette.txt", "r") as f:
+            for line in f.readlines():
+                raw_song_list.append(line.rstrip("\n"))
 
-    song_dict = []
-    for song in raw_song_list:
-        song_line_split = song.split(" - ")
-        song_dict.append({
-            "Song Name": song_line_split[0],
-            "Artist": song_line_split[1],
-            "Duration": song_line_split[2],
-        })
-    return song_dict
+        song_dict = []
+        for song in raw_song_list:
+            song_line_split = song.split(" - ")
+            song_dict.append({
+                "Song Name": song_line_split[0],
+                "Artist": song_line_split[1],
+                "Duration": song_line_split[2],
+            })
+        return song_dict
 
 def main():
     kara = Roulette()
